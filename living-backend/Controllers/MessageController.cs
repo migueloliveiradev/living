37eedf1dac41f759e6252b8a612c0e78f2e18d64
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-09-01 15:12:53 -0300

    Inicio parte mensagem

---
+++ living-backend/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using living_backend.DTOs.Request.Messages;
+using living_backend.Models.Messages;
+using living_backend.Repositories.Messages;
+using living_backend.Shared.Extensions.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace living_backend.Controllers;
+public class MessageController : ControllerBase
+{
+    private readonly IMessageRepository messageRepository;
+    public MessageController(IMessageRepository messageRepository)
+    {
+        this.messageRepository = messageRepository;
+    }
+    [HttpGet("/messages/{username}")]
+    public IActionResult GetMessages(string username, int skip, int take)
+    {
+        Task.Delay(2000).Wait();
+        string username_me = User.GetUsername();
+        List<Message> messages = messageRepository.GetMessages(username, username_me, skip, take);
+        return Ok(messages);
+    }
+    [HttpPost("/messages/send")]
+    public IActionResult SendMessage(MessageRequest messageRequest)
+    {
+        if(!ModelState.IsValid)
+        {
+            return UnprocessableEntity(ModelState);
+        }
+        Message message = messageRequest.ToModel();
+        message.SenderId = User.GetId();
+        message.CreatedAt = DateTime.Now;
+        message.IsRead = false;
+        messageRepository.Create(message);
+        return Ok(message);
+    }
+}
---
+++ living-backend/Repositories/Messages/MessageRepository.cs
@@ -0,0 +1,49 @@
+﻿using living_backend.Database;
+using living_backend.Hubs;
+using living_backend.Models.Messages;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace living_backend.Repositories.Messages;
+
+public class MessageRepository : IMessageRepository
+{
+    private readonly DatabaseContext context;
+    private readonly IHubContext<ChatHub> hubContext;
+    public MessageRepository(DatabaseContext context, IHubContext<ChatHub> hubContext)
+    {
+        this.context = context;
+        this.hubContext = hubContext;
+    }
+    public Message? GetById(int id)
+    {
+        return context.Messages.Find(id);
+    }
+
+    public List<Message> GetMessages(string username, string username_me, int skip = 0, int take = 20)
+    {
+        return context.Messages
+            .Include(m => m.Sender)
+            .Include(m => m.Receiver)
+            .Where(m => m.Sender.Username == username 
+                || m.Sender.Username == username_me
+                || m.Receiver.Username == username
+                || m.Receiver.Username == username_me)
+            .OrderByDescending(m => m.CreatedAt)
+            .Skip(skip)
+            .Take(take)
+            .ToList();
+    }
+
+    public Message Create(Message message)
+    {
+        context.Messages.Add(message);
+        context.SaveChanges();
+        return message;
+    }
+
+    public void ReadAllConversation(int id_user)
+    {
+        context.Messages.Where(m => m.SenderId == id_user).ExecuteUpdate(m => m.SetProperty(x => x.IsRead, true));
+    }
+}
