f4fa11677cda746f053aa4b549bf6e1ae63c3fc1
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-09-01 15:17:20 -0300

    Inicio parte notificações

---
+++ living-backend/Hubs/NotificationHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace living_backend.Hubs;
+
+public class NotificationHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
