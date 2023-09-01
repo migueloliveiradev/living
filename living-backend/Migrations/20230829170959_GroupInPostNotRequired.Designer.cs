9c16adfba06276f534bc9734d1e0bbb4a28784f3
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-09-01 15:19:00 -0300

    Inicio parte grupos

--- living-backend/Migrations/20230829170959_GroupInPostNotRequired.Designer.cs
+++ living-backend/Migrations/20230829170959_GroupInPostNotRequired.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using living_backend.Database;
@@ -11,9 +12,11 @@ using living_backend.Database;
 namespace living_backend.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230829170959_GroupInPostNotRequired")]
+    partial class GroupInPostNotRequired
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
