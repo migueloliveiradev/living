a8badb52d17329156c6be12e1eeba68abff3232f
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-09-01 15:13:39 -0300

    Extensão para configuração da injeção de dependencia

---
+++ living-backend/Config/ConfigDependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using living_backend.Repositories.Messages;
+using living_backend.Repositories.Posts;
+using living_backend.Repositories.Users;
+using living_backend.Services.Users;
+
+namespace living_backend.Config;
+
+public static class ConfigDependencyInjection
+{
+    public static IServiceCollection ConfigureDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IPostRepository, PostRepository>();
+        //services.AddScoped<IGroupRepository, GroupRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<UserLoginService>();
+        //services.AddScoped<INotificationRepository, NotificationRepository>();
+        return services;
+    }
+}
