e668da53eee5cedb16860087b2a4ea653fc3e85f
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2024-02-02 19:54:55 -0300

    feat: create tests user login

--- Living.Tests/Auth/TestLoginUser.cs
+++ Living.Tests/Auth/TestLoginUser.cs
@@ -1,4 +1,5 @@
 ﻿using Living.Application.UseCases.Users.Login;
+using Living.Domain.Entities.Users.Constants;
 using Living.Domain.Entities.Users.Models;
 using System.Net;
 using System.Net.Http.Json;
@@ -48,5 +49,42 @@ public class TestLoginUser : SetupWebAPI
         cookies.Should().HaveCount(1);
         var cookie = cookies.First();
         cookie.Should().StartWith(".AspNetCore.Identity.Application=");
+    }
+
+    [Fact]
+    public async Task ShouldNotLoginUserWhenUserNotInvalid()
+    {
+        var registerUserCommand = RegisterUserFaker.Instance.Generate();
+        var response = await client.PostAsJsonAsync("/api/auth/login", new LoginUserCommand
+        {
+            Email = registerUserCommand.Email,
+            Password = registerUserCommand.Password,
+            UseCookies = false
+        });
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        var content = await response.Content.ReadFromJsonAsync<BaseResponse>();
+        content!.Notifications.Should().ContainKey("USER");
+        content.Notifications["USER"].Should().Contain(UserErrors.USER_NOT_FOUND.Code);
+    }
+
+    [Fact]
+    public async Task ShouldNotLoginUserWhenPasswordIsInvalid()
+    {
+        var registerUserCommand = RegisterUserFaker.Instance.Generate();
+        var responseRegister = await client.PostAsJsonAsync("/api/auth/register", registerUserCommand);
+        responseRegister.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var response = await client.PostAsJsonAsync("/api/auth/login", new LoginUserCommand
+        {
+            Email = registerUserCommand.Email,
+            Password = "invalid-password",
+            UseCookies = false
+        });
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var content = await response.Content.ReadFromJsonAsync<BaseResponse>();
+        content!.Notifications.Should().ContainKey("USER");
+        content.Notifications["USER"].Should().Contain(UserErrors.USER_PASSWORD_INVALID.Code);
     }
 }
