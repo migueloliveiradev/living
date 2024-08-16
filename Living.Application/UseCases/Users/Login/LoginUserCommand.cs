94122e423f904ae578fac8887e1e5d757cd765e2
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2024-08-16 17:23:29 -0300

    feat: remove package FluentValidation

--- Living.Application/UseCases/Users/Login/LoginUserCommand.cs
+++ Living.Application/UseCases/Users/Login/LoginUserCommand.cs
@@ -1,20 +1,6 @@
-﻿using FluentValidation;
-
-namespace Living.Application.UseCases.Users.Login;
+﻿namespace Living.Application.UseCases.Users.Login;
 public class LoginUserCommand : IRequest<BaseResponse>
 {
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
-}
-
-public class LoginCommandValidator : AbstractValidator<LoginUserCommand>
-{
-    public LoginCommandValidator()
-    {
-        RuleFor(x => x.Email)
-            .NotEmpty().WithErrorCode("IS_REQUIRED")
-            .EmailAddress().WithErrorCode("IS_EMAIL");
-        RuleFor(x => x.Password)
-            .NotEmpty().WithErrorCode("IS_REQUIRED");
-    }
 }
